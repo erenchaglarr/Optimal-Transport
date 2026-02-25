468954db21b66afeeb19a0e0f7a32444804f0284
erenchaglarr <s245735@dtu.dk>
2026-02-25 14:20:37 +0100

    Initializing commit

---
+++ src/optimaltransport/train.py
@@ -0,0 +1,10 @@
+from optimaltransport.model import Model
+from optimaltransport.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
