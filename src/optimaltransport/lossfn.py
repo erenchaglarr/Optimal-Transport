67f47e5fa387eb3d736e4202a3583e639e4ab18a
LordCapitalist <Github@horizon0.anonaddy.com>
2026-06-08 11:50:20 +0200

    Added L2 regularization

--- src/optimaltransport/lossfn.py
+++ src/optimaltransport/lossfn.py
@@ -8,6 +8,17 @@ def torch_batch_to_jax(x):
     return jnp.asarray(x.detach().cpu().numpy(), dtype=jnp.float32)
 
 ## This is the MSE loss function.
-def reconstruction_mse_loss(model, x_batch):
-    x_hat_batch = jax.vmap(model)(x_batch)
-    return jnp.mean((x_hat_batch - x_batch) ** 2)
+#def reconstruction_mse_loss(model, x_batch):
+    #x_hat_batch = jax.vmap(model)(x_batch)
+    #return jnp.mean((x_hat_batch - x_batch) ** 2)
+
+def reconstruction_mse_loss(model, x_batch, latent_l2_weight=1e-3):
+    z_batch = jax.vmap(model.encoder)(x_batch)
+    x_hat_batch = jax.vmap(model.decoder)(z_batch)
+
+    rec_loss = jnp.mean((x_hat_batch - x_batch) ** 2)
+    latent_l2 = jnp.mean(jnp.sum(z_batch ** 2, axis=-1))
+
+    total_loss = rec_loss + latent_l2_weight * latent_l2
+
+    return total_loss
